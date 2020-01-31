c3399bc0ea7bcf29caa270797c18e1fade60ca49
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-01-31 14:57:24 +0530

    Added: App Class

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 const http = require('http');
-const {stdout} = process;
+const {stdout, stderr} = process;
 
-const {processRequest} = require('./app');
+const {app} = require('./lib/handlers');
 const PORT = 4000;
 
-const requestListener = function (req, res) {
-  const remote = {addr: req.socket.remoteAddress, port: req.socket.remotePort};
-  stdout.warn('New Connection: ', remote);
-
-  req.on('close', (hadError) => {
-    stdout.warn(remote, 'closed', `${hadError ? 'with error.' : ''}`);
-  });
-
-  req.on('end', () => stdout.warn(remote, 'ended'));
-
-  req.on('error', (error) => stdout.error(`Socket Error ${error}`));
-
-  processRequest(req, res);
-};
-
 const main = function () {
-  const server = http.Server(requestListener);
-  server.on('error', (error) => stdout.error(error));
-  server.listen(PORT, () => stdout.log(`Listening at ${PORT}`));
+  const server = new http.Server(app.serve.bind(app));
+
+  server.on('error', (error) => stderr.write(error));
+  server.listen(PORT, () => stdout.write(`Listening at ${PORT}`));
 };
 
 main();
